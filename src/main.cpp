f83548896ee48b8aee5823264d22a6aa2a539c2b
Ubuntu <ubuntu@feabhas.com>
2017-02-27 11:17:47 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+// ------------------------------------------------------------
+// FEABHAS LTD
+// Transitioning to C++11 / C++14
+// AC++11-401
+//
+// main.cpp
+//
+// ------------------------------------------------------------
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  cout << "Modern C++"  << endl;
+}
